4f75c01fee4881b2b034ebdf4a415e1ce44f1123
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-12 11:53:24 -0500

    make 3 casos

--- makefile_difusion.mk
+++ makefile_difusion.mk
@@ -6,16 +6,16 @@ caso1.png: inicial.txt futura.txt futura2.txt Plots_Difusion.py Difusion.cpp
 a.out: Difusion.cpp
 	g++ Difusion.cpp
 
-#caso2.png: inicialc2.txt futurac2.txt futura2c2.txt Plots_Difusion.py Difusion.cpp
-	#python Plots_Difusion.py
-#%.txt: a.out
-	#./a.out
-#a.out: Difusion.cpp
-	#g++ Difusion.cpp
+caso2.png: inicialc2.txt futurac2.txt futura2c2.txt Plots_Difusion.py Difusion.cpp
+	python Plots_Difusion.py
+%.txt: a.out
+	./a.out
+a.out: Difusion.cpp
+	g++ Difusion.cpp
 
-#caso3.png: inicialc3.txt futurac3.txt futura2c3.txt Plots_Difusion.py Difusion.cpp
-	#python Plots_Difusion.py
-#%.txt: a.out
-	#./a.out
-#a.out: Difusion.cpp
-	#g++ Difusion.cpp
+caso3.png: inicialc3.txt futurac3.txt futura2c3.txt Plots_Difusion.py Difusion.cpp
+	python Plots_Difusion.py
+%.txt: a.out
+	./a.out
+a.out: Difusion.cpp
+	g++ Difusion.cpp
