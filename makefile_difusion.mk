04c9d1f9f51678488c05f09698e5a96da63029c5
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-11 22:15:13 -0500

    make fijas

---
+++ makefile_difusion.mk
@@ -0,0 +1,21 @@
+#makefile
+caso1.png: inicial.txt futura.txt futura2.txt Plots_Difusion.py Difusion.cpp
+	python Plots_Difusion.py
+%.txt: a.out
+	./a.out
+a.out: Difusion.cpp
+	g++ Difusion.cpp
+
+#caso2.png: inicialc2.txt futurac2.txt futura2c2.txt Plots_Difusion.py Difusion.cpp
+	#python Plots_Difusion.py
+#%.txt: a.out
+	#./a.out
+#a.out: Difusion.cpp
+	#g++ Difusion.cpp
+
+#caso3.png: inicialc3.txt futurac3.txt futura2c3.txt Plots_Difusion.py Difusion.cpp
+	#python Plots_Difusion.py
+#%.txt: a.out
+	#./a.out
+#a.out: Difusion.cpp
+	#g++ Difusion.cpp
